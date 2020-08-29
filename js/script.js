82b58ea29d2ce8af2e06b20d70ffb60953f05c25
Jen Speak <jspeak6@gmail.com>
2020-08-29 13:36:50 -0700

    Added functionality to share button click

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const shareBtnInactive = document.querySelector('.card__share-icon');
+const shareBtnActive = document.querySelector('.card__share-icon--active');
+const shareActive = document.querySelector('.card__share');
+
+shareBtnInactive.addEventListener('click', () => {
+    shareActive.classList.toggle('card__share--visible');
+});
+
+shareBtnActive.addEventListener('click', () => {
+    shareActive.classList.toggle('card__share--visible');
+});
