4b55a4ea62878df292c28e92927afa9de1676306
Jen Speak <jspeak6@gmail.com>
2020-08-30 17:03:18 -0700

    Added media queries

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
-const shareBtnInactive = document.querySelector('.card__share-icon');
-const shareBtnActive = document.querySelector('.card__share-icon--active');
+const shareBtn = document.querySelector('.card__share-icon');
 const shareActive = document.querySelector('.card__share');
 
-shareBtnInactive.addEventListener('click', () => {
-    shareActive.classList.toggle('card__share--visible');
-});
-
-shareBtnActive.addEventListener('click', () => {
+shareBtn.addEventListener('click', () => {
     shareActive.classList.toggle('card__share--visible');
 });
